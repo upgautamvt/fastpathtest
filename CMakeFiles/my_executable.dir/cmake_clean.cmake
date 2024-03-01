9559f9ec563e36085f92fb1de92aded20092ec7e
upgautamvt <upgautam@vt.edu>
2024-03-01 18:57:31 -0500

    fastpath setup

---
+++ CMakeFiles/my_executable.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_executable.dir/HelloWorld.c.o"
+  "CMakeFiles/my_executable.dir/HelloWorld.c.o.d"
+  "my_executable"
+  "my_executable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/my_executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/my_executable_sanitized.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for my_executable_sanitized.
+# This may be replaced when dependencies are built.
