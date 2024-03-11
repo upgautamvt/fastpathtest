480af2b9797e2bbf34fc66b87a1128ca35c3349f
upgautamvt <upgautam@vt.edu>
2024-03-11 18:24:20 -0400

    vt lab work version 1

--- CMakeFiles/HelloWorld.dir/DependInfo.cmake
+++ CMakeFiles/HelloWorld.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/upgautam/CLionProjects/FastPathTest/HelloWorld.c" "CMakeFiles/HelloWorld.dir/HelloWorld.c.o" "gcc" "CMakeFiles/HelloWorld.dir/HelloWorld.c.o.d"
+  "/home/upgautam/CLionProjects/fastpathtest/HelloWorld.c" "CMakeFiles/HelloWorld.dir/HelloWorld.c.o" "gcc" "CMakeFiles/HelloWorld.dir/HelloWorld.c.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
