01025c1c3551bd593a7edfcb04b995be15f72d47
upgautamvt <upgautam@vt.edu>
2024-03-13 00:38:00 -0400

    added MillionLoop and ThousandLoop C files

---
+++ ThousandLoop.c
@@ -0,0 +1,16 @@
+//
+// Created by upgautam on 3/13/24.
+//
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    int n, i, sum=0;
+    for (i=0; i<1000; i++) {
+        n=getpid();
+        sum=sum+n;
+    }
+    printf("Answer is %d\n", sum);
+    return 0;
+}
